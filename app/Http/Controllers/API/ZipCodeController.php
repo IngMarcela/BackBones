034a1884284e451fed80a6fc0a6a3110e0066457
Marcela Dominguez <marceladominguez1993@gmail.com>
2023-03-09 23:48:17 -0500

    refactor: best to data

--- app/Http/Controllers/API/ZipCodeController.php
+++ app/Http/Controllers/API/ZipCodeController.php
@@ -12,34 +12,67 @@ class ZipCodeController extends Controller
 {
     public function getZipCode($zip_code): JsonResponse
     {
-        $code = Code::where('d_codigo', $zip_code)->first();
-        if ($code) {
-            $settlements = [];
-            $locality = strtoupper(Str::ascii($code->d_ciudad));
-            $federal_entity = [
-                'key' => (int) $code->c_estado,
-                'name' => Str::upper(ucwords(Str::ascii($code->d_estado))),
-                'code' => $code->c_cp ?: null
-            ];
-            $municipality = [
-                'key' => (int) $code->c_mnpio,
-                'name' => Str::upper(Str::ascii($code->d_mnpio))
-            ];
-
-            $settlements[] = [
-                'key' => (int) $code->id_asenta_cpcons,
-                'name' => Str::upper(ucwords(Str::ascii($code->d_asenta))),
-                'zone_type' => Str::upper(ucwords(Str::ascii($code->d_zona))),
-                'settlement_type' => [
-                    'name' => ucwords($code->d_tipo_asenta)
-                ]
-            ];
+        $codes = Code::where('d_codigo', $zip_code)->get();
+        if ($codes) {
+            $settlements = array();
+            //
+            $locality = '';
+            // federal entiy info
+            $federal_entity_key = 0;
+            $federal_entity_code = 0;
+            $federal_entity_name = '';
+            // municipality info
+            $municipality_key = 0;
+            $municipality_name = '';
+            //
+            $head_filled = false;
+            // loop zip code list
+            foreach ($codes as $code) {
+                //
+                if (! $head_filled) {
+                    $locality = strtoupper(Str::ascii($code['d_ciudad']));
+                    //
+                    $federal_entity_key = (int) $code['c_estado'];
+                    $federal_entity_name = strtoupper(Str::ascii($code['d_estado']));
+                    $federal_entity_code = ! $code['c_cp'] ? null : $code['c_cp'];
+                    //
+                    $municipality_key = (int) $code['c_mnpio'];
+                    $municipality_name = strtoupper(Str::ascii($code['d_mnpio']));
+                    $head_filled = true;
+                }
+                //
+                $settlement_key = (int) $code['id_asenta_cpcons'];
+                $settlement_name = strtoupper(Str::ascii($code['d_asenta']));
+                $settlement_zone_type = strtoupper(Str::ascii($code['d_zona']));
+                $settlement_type_name = $code['d_tipo_asenta'];
+                //
+                $settlements[] = array(
+                    "key" => $settlement_key,
+                    "name" => $settlement_name,
+                    "zone_type" => $settlement_zone_type,
+                    "settlement_type" => array(
+                        "name" => $settlement_type_name
+                    )
+                );
+            }
+            //
+            $federal_entity = array(
+                "key" => $federal_entity_key,
+                "name" => $federal_entity_name,
+                "code" => $federal_entity_code
+            );
+            //
+            $municipality = array(
+                "key" => $municipality_key,
+                "name" => $municipality_name
+            );
+            //
             $results = [
-                'zip_code' => $zip_code,
-                'locality' => $locality,
-                'federal_entity' => $federal_entity,
-                'settlements' => $settlements,
-                'municipality' => $municipality
+                "zip_code" => $zip_code,
+                "locality" => $locality,
+                "federal_entity" => $federal_entity,
+                "settlements" => $settlements,
+                "municipality" => $municipality
             ];
         } else {
             $results = $this->errorResponse();
